2654961deb3385359c82c94121e0f6100cc3edfd
Paula Rojas <pa.rojaso@duocuc.cl>
2026-05-15 00:02:50 -0400

    "Configura CORS para Vercel en backend pago"

--- backend_pago/src/main/java/backend_pago/Config/corsGlobalConfig.java
+++ backend_pago/src/main/java/backend_pago/Config/corsGlobalConfig.java
@@ -5,8 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
-
 @Configuration
 public class corsGlobalConfig {
 
@@ -15,8 +13,11 @@ public class corsGlobalConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")  
-                        .allowedOrigins("http://localhost:5173") 
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                            "http://localhost:5173",
+                            "https://frontend-portafolio-lumiskin-yebo.vercel.app"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
