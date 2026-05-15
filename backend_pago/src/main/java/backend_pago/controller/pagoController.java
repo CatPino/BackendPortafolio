61fa9c190db64bc7f813485b4b6d106ecd36921c
Paula Rojas <pa.rojaso@duocuc.cl>
2026-05-14 23:33:23 -0400

    Corrige getImagenUrl en backend pago

--- backend_pago/src/main/java/backend_pago/controller/pagoController.java
+++ backend_pago/src/main/java/backend_pago/controller/pagoController.java
@@ -62,7 +62,7 @@ public class pagoController {
             det.setProducto(d.getProducto());
             det.setCantidad(d.getCantidad());
             det.setPrecioUnitario(d.getPrecioUnitario());
-            det.setImagenUrl(d.getimagenUrl());
+            det.setImagenUrl(d.getImagenUrl());
 
             double sub = d.getCantidad() * d.getPrecioUnitario(); // precio YA incluye IVA
             det.setSubtotal(sub);
